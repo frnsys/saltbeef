005e0b2c909ce3790ff94881b32eda7a29be2223
Francis Tseng <f+accounts@frnsys.com>
2015-08-07 23:03:56 -0400

    for some reason need to specify encoding

--- saltbeef/generate/data.py
+++ saltbeef/generate/data.py
@@ -2,7 +2,7 @@ from glob import glob
 
 
 def load_lexicon(fname):
-    with open(fname, 'r') as f:
+    with open(fname, 'r', encoding='utf8') as f:
         return [l.lower().strip() for l in f.readlines()]
 
 def load_lexicons(pattern):
